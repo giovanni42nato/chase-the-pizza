fd2d80db5d29133a4b690e227489198adb96e311
giovanni42nato <142410244+giovanni42nato@users.noreply.github.com>
2023-08-16 09:43:04 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+ 
+scenlet mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function(sprite: Sprite, otherSprite: Sprite) {
+    
+})
+`, SpriteKind.Player)e.setBackgroundColor(0)
